cec973b0bf615921fa3b395eaaf8ec1128e7c848
hyeonjaez <jhjh0022@naver.com>
2025-04-15 16:27:12 +0900

    feat: rest controller advice

---
+++ src/main/java/com/example/dosirakbe/global/exception/ExceptionAdvice.java
@@ -0,0 +1,19 @@
+package com.example.dosirakbe.global.exception;
+
+import com.github.hyeonjaez.springcommon.handler.AbstractGlobalExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * packageName    : com.example.dosirakbe.global.handler<br>
+ * fileName       : ExceptionAdvice.java<br>
+ * author         : Fiat_lux<br>
+ * date           : 25. 4. 15.<br>
+ * description    :  <br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 25.04.15          Fiat_lux           최초생성<br>
+ */
+@RestControllerAdvice
+public class ExceptionAdvice extends AbstractGlobalExceptionHandler {
+}
